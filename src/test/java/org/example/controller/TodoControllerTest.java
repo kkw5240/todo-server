72a07e66f7e3dc0aa2e7a416c31cc4fe27dddf41
kkw5240 <kkw5240@gmail.com>
2021-04-28 00:02:42 +0900

    ToDo API Server Test Code 작성 중

--- src/test/java/org/example/controller/TodoControllerTest.java
+++ src/test/java/org/example/controller/TodoControllerTest.java
@@ -1,10 +1,8 @@
 package org.example.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.model.TodoEntity;
 import org.example.model.TodoRequest;
-import org.example.model.TodoResponse;
 import org.example.service.TodoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,12 +12,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -89,11 +88,53 @@ class TodoControllerTest {
     }
 
     @Test
-    void readAll() {
+    void readAll() throws Exception {
+        List<TodoEntity> expectedTodoes = new ArrayList<>();
+        expectedTodoes.add(this.expected);
+
+        when(this.todoService.searchAll())
+                .thenReturn(expectedTodoes);
+
+        TodoRequest request = new TodoRequest();
+        request.setTitle("Any Title");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(request);
+
+        this.mockMvc.perform(
+                get("/").contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value("Test Title"))
+                .andExpect(jsonPath("$[0].order").value(0L))
+                .andExpect(jsonPath("$[0].completed").value(false));
     }
 
     @Test
-    void updateOne() {
+    void updateOne() throws Exception {
+        when(this.todoService.updateById(anyLong(), any(TodoRequest.class)))
+                .then((i) -> {
+                    TodoRequest request = i.getArgument(0, TodoRequest.class);
+                    return new TodoEntity(
+                            this.expected.getId(),
+                            request.getTitle(),
+                            this.expected.getOrder(),
+                            this.expected.getCompleted());
+                });
+
+        TodoRequest request = new TodoRequest();
+        request.setTitle("Any Title");
+        request.setOrder(1L);
+        request.setCompleted(false);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(request);
+
+        this.mockMvc.perform(
+                patch("/{id}", 123L).contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Any Title"));
     }
 
     @Test
